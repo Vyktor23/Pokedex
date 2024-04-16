a4272cb86de2be5b02fe58a88cdeac1f3dc22c01
Víctor Ramírez <146446586+Vyktor23@users.noreply.github.com>
2024-04-15 21:50:30 -0500

    Update pokedex.vue

--- src/components/pokedex.vue
+++ src/components/pokedex.vue
@@ -6,230 +6,265 @@ let pokemon = ref([]);
 let buscador = ref("");
 
 function random() {
-	let numero = Math.floor(Math.random() * 1025) + 1;
-	traer(numero);
+    let numero = Math.floor(Math.random() * 1025) + 1;
+    traer(numero);
 }
 
 function buscar() {
-	traer(buscador.value);
+    traer(buscador.value);
 }
 
 async function traer(numero) {
-	try {
-		let r = await axios.get("https://pokeapi.co/api/v2/pokemon/" + numero);
-		pokemon.value = r.data;
-		console.log(r.data);
-	} catch (error) {
-		console.log(error);
-	}
+    try {
+        let r = await axios.get("https://pokeapi.co/api/v2/pokemon/" + numero);
+        pokemon.value = r.data;
+        console.log(r.data);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+// Función para obtener la clase de color asociada a cada tipo de Pokémon
+function getTypeClass(type) {
+    switch (type) {
+        case 'normal':
+            return 'type-normal';
+        case 'fire':
+            return 'type-fire';
+        case 'water':
+            return 'type-water';
+        case 'electric':
+            return 'type-electric';
+        case 'grass':
+            return 'type-grass';
+        case 'ice':
+            return 'type-ice';
+        case 'fighting':
+            return 'type-fighting';
+        case 'poison':
+            return 'type-poison';
+        case 'ground':
+            return 'type-ground';
+        case 'flying':
+            return 'type-flying';
+        case 'psychic':
+            return 'type-psychic';
+        case 'bug':
+            return 'type-bug';
+        case 'rock':
+            return 'type-rock';
+        case 'ghost':
+            return 'type-ghost';
+        case 'dragon':
+            return 'type-dragon';
+        case 'dark':
+            return 'type-dark';
+        case 'steel':
+            return 'type-steel';
+        case 'fairy':
+            return 'type-fairy';
+        default:
+            return 'type-default';
+    }
 }
 </script>
 
 <template>
 	<div id="contenidoMain">
-		<section class="buscador">
-			<!-- buscador -->
-			<input type="text" placeholder="Número o nombre del Pokémon..." v-model="buscador" id="texto" />
-			<button @click="buscar()">Consultar</button>
-			<button @click="random()">Impresióname</button>
-		</section>
-
-		<section class="justify-center card">
-			<!-- tarjeta -->
-			<div class="parte1">
-				<div v-if="pokemon.id">
-					ID:
-					{{ pokemon.id }}
-				</div>
-				<div>
-					{{ pokemon.name }}
-				</div>
-				<img :src="pokemon.sprites.other['official-artwork'].front_default" alt="" v-if="pokemon.sprites" class="imagen_principal" />
-				<div>
-					<img :src="pokemon.sprites.other.showdown.front_default" alt="" v-if="pokemon.sprites" />
-					<img :src="pokemon.sprites.other.showdown.back_default" alt="" v-if="pokemon.sprites" />
-				</div>
+	  <section class="buscador">
+		<!-- buscador -->
+		<input type="text" placeholder="Número o nombre del Pokémon..." v-model="buscador" id="texto" />
+		<button @click="buscar()">Consultar</button>
+		<button @click="random()">Impresióname</button>
+	  </section>
+  
+	  <section class="card">
+		<!-- tarjeta -->
+		<div class="parte1">
+		  <div class="pokemon-id" v-if="pokemon.id">
+			<span class="info-label">ID:</span>
+			<span>{{ pokemon.id }}</span>
+		  </div>
+		  <div class="pokemon-name">
+			{{ pokemon.name }}
+		  </div>
+		  <img :src="pokemon.sprites.other['official-artwork'].front_default" alt="" v-if="pokemon.sprites" class="imagen_principal" />
+		  <div class="sprites-container">
+			<img :src="pokemon.sprites.other.showdown.front_default" alt="" v-if="pokemon.sprites" />
+			<img :src="pokemon.sprites.other.showdown.back_default" alt="" v-if="pokemon.sprites" />
+		  </div>
+		</div>
+  
+		<div class="parte2">
+		  <section id="sub2">
+			<div v-for="(e, i) in pokemon.types" :key="i">
+			  <span class="info-label">Type:</span>
+			  <span class="type" :class="getTypeClass(e.type.name)">{{ e.type.name }}</span>
 			</div>
-
-			<div class="parte2">
-				<section id="sub2">
-					<div v-for="(e, i) in pokemon.types" :key="i">
-						{{ e.type.name }}
-					</div>
-					<div v-if="pokemon.base_experience">
-						Exp. Base:
-						{{ pokemon.base_experience }}
-					</div>
-					<div v-if="pokemon.height">
-						Height:
-						{{ pokemon.height }}
-					</div>
-					<div v-if="pokemon.weight">
-						Weight:
-						{{ pokemon.weight }}
-					</div>
-				</section>
-				<section class="linear-progress-section" id="subb2">
-					<!-- Sección de barras de progreso -->
-					<div class="linear-progress" v-for="(e, i) in pokemon.stats" :key="i">
-						<label :for="'ID' + i" rounded size="14px" color="purple" class="stat-name text-xs font-semibold text-black">
-							{{ e.stat.name }}
-						</label>
-						<q-linear-progress :id="'ID${i}' + i" :value="Number(`0.${e.base_stat}`)" :buffer="Number(`0.${e.base_stat + 10}`)" rounded size="25px" color="blue">
-							<div class="absolute-full flex flex-center">
-								<q-badge color="white" text-color="accent" :label="e.base_stat" />
-							</div>
-						</q-linear-progress>
-					</div>
-				</section>
+			<div v-if="pokemon.base_experience">
+			  <span class="info-label">Exp. Base:</span>
+			  <span class="value">{{ pokemon.base_experience }}</span>
 			</div>
-		</section>
+			<div v-if="pokemon.height">
+			  <span class="info-label">Height:</span>
+			  <span class="value">{{ pokemon.height }}</span>
+			</div>
+			<div v-if="pokemon.weight">
+			  <span class="info-label">Weight:</span>
+			  <span class="value">{{ pokemon.weight }}</span>
+			</div>
+		  </section>
+		  <section class="linear-progress-section" id="subb2">
+			<!-- Sección de barras de progreso -->
+			<div class="linear-progress" v-for="(e, i) in pokemon.stats" :key="i">
+			  <label class="stat-name">{{ e.stat.name }}:</label>
+			  <div class="progress-bar-container">
+				<div class="progress-bar" :style="{ width: `${e.base_stat / 1}px` }">{{ e.base_stat }}</div>
+			  </div>
+			</div>
+		  </section>
+		</div>
+	  </section>
 	</div>
-</template>
+  </template>
 
 <style scoped>
 #contenidoMain {
-	display: flex;
-	align-items: center;
-}
-
-.pokedex-container {
-	width: 100%;
-	height: 100%;
-	display: flex;
-	flex-direction: column;
-	background-color: white;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: -100px;
 }
 
 .buscador {
-	text-align: center;
-	padding: 10px;
-	display: flex;
-	flex-direction: row;
-	gap: 15px;
-	position: absolute;
-	top: 90px;
+  text-align: center;
+  padding: 10px;
+  display: flex;
+  flex-direction: row;
+  gap: 15px;
+  margin-top: 20px;
+}
+
+
+.pokemon-id {
+  margin-bottom: 5px;
+  font-size: 20px;
+  font-weight: bold;
+}
+
+.pokemon-name {
+  font-size: 26px;
+  font-weight: bold;
+  color: #333;
 }
 
 #texto {
-	width: 240px;
-}
-
-.imagen_principal {
-	width: 250px;
-	height: 250px;
+  width: 240px;
 }
 
 .card {
-	background-color: white;
-	display: flex;
-	flex-direction: row;
-	width: 100%;
-	height: 87.7vh;
-	color: black;
-	gap: 30px;
-	text-align: center;
+  background-color: white;
+  display: flex;
+  flex-direction: row;
+  width: 80%;
+  height: 100%;
+  margin-top: 20px;
+  padding: 30px;
+  border-radius: 70px;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+  color: black;
+  font-size: 16px;
 }
 
 .parte1 {
-	display: flex;
-	flex-direction: column;
-	justify-content: center;
-	align-content: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  flex: 1;
+}
+
+.parte1 .info-label {
+  font-weight: bold;
+}
+
+.parte1 .imagen_principal {
+  width: 250px;
+  height: 250px;
+  margin-top: 20px;
+}
+
+.sprites-container {
+  display: flex;
+  gap: 80px;
+  margin-top: 40px;
 }
 
 .parte2 {
-	display: flex;
-	flex-direction: row;
-	justify-content: center;
-	align-content: center;
-	gap: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  flex: 1;
 }
 
-#sub2 {
-	display: flex;
-	flex-direction: column;
-	justify-content: center;
-	align-content: center;
+.parte2 .info-label {
+  font-weight: bold;
+  padding: 3px 8px;
+  border-radius: 10px;
 }
 
-#subb2 {
-	display: flex;
-	flex-direction: column;
-	justify-content: center;
-	align-content: center;
+
+.value {
+  margin-left: 5px;
+  padding: 3px 8px;
+  border-radius: 10px;
+  font-size: 16px;
+  background-color: rgb(126, 192, 212);
 }
 
-/* .parte1 {
-	height: 30%;
-	width: 20%;
-	font-size: 15px;
-	position: absolute;
-	top: 13rem;
-	left: 22rem;
+.type {
+  margin-left: 5px;
+  padding: 3px 8px;
+  border-radius: 10px;
+  font-size: 16px;
 }
 
-.parte2 {
-	width: 30%;
-	display: flex;
-	flex-direction: column;
-	justify-content: center;
-	align-items: center;
+
+.type-normal { background-color: #A8A878; }
+.type-fire { background-color: #F08030; }
+.type-water { background-color: #6890F0; }
+.type-electric { background-color: #F8D030; }
+.type-grass { background-color: #78C850; }
+.type-ice { background-color: #98D8D8; }
+.type-fighting { background-color: #C03028; }
+.type-poison { background-color: #A040A0; }
+.type-ground { background-color: #E0C068; }
+.type-flying { background-color: #A890F0; }
+.type-psychic { background-color: #F85888; }
+.type-bug { background-color: #A8B820; }
+.type-rock { background-color: #B8A038; }
+.type-ghost { background-color: #705898; }
+.type-dragon { background-color: #7038F8; }
+.type-dark { background-color: #705848; }
+.type-steel { background-color: #B8B8D0; }
+.type-fairy { background-color: #EE99AC; }
+
+.linear-progress-section {
+  margin-top: 40px;
 }
 
-.parte2 div {
-	display: flex;
-	flex-direction: row;
-	align-items: center;
+.progress-bar-container {
+  width: 100%;
+  height: 100%;
+  background-color: #f0f0f0;
+  border-radius: 5px;
+  margin-bottom: 6px;
 }
 
-.parte2 div img {
-	height: 60px;
-}
-
-.parte3 {border: 1px solid green;
-	width: 20%;
-	height: 70%;
-}
-
-.parte4 {
-	width: 20%;
-	height: 70%;
-}
-
-.parte4 section div {
-	width: 0px;
-	height: 10px;
-	display: flex;
-	gap: 30px;
-} */
-
-.search-section {
-	display: grid;
-	gap: 15px;
-	margin-bottom: 50px;
-	margin-top: 30px;
-	font-size: 18px;
-}
-
-.stats-section {
-	margin-top: 10px;
-}
-
-.stat-name {
-	color: #333;
-	font-size: 18px;
-}
-
-.linear-progress {
-	margin-bottom: 10px;
-}
-
-.bg-gradient {
-	background: linear-gradient(to right, #673ab7, #2196f3);
-}
-
-.linear-progress {
-	text-align: center;
+.progress-bar {
+  height: 100%;
+  background-color: #4caf50;
+  border-radius: 5px;
 }
 </style>
