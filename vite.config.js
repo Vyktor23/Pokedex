32664017a11a5ed5505ba1f5ed12b97bc44a1d4e
Vyktor23 <ramirezrangelvictor2@gmail.com>
2026-06-16 15:47:33 -0500

    actualizacion versiones

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
+import { fileURLToPath, URL } from 'node:url'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue({
-      template: { transformAssetUrls }
+      template: { transformAssetUrls },
     }),
-
-    // @quasar/plugin-vite options list:
-    // https://github.com/quasarframework/quasar/blob/dev/vite-plugin/index.d.ts
     quasar({
-      sassVariables: 'src/quasar-variables.sass'
-    })
-  ]
+      sassVariables: fileURLToPath(
+        new URL('./src/quasar-variables.sass', import.meta.url),
+      ),
+    }),
+  ],
 })
